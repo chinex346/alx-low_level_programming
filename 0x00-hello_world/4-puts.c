15e00dac4456dfaff826aaf36c7b097c8e80eae7
chinex346 <chinemeremotuokere@gmail.com>
2022-09-08 16:22:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+i#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * Reeturn: 0 if exited properly non-zero other
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
